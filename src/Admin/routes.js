6a947f250dc6fdcf31a4bf10654f57d08e574b39
lac2304 <loihuu114@gmail.com>
2025-11-11 15:33:57 +0700

    page promotion setting

--- src/Admin/routes.js
+++ src/Admin/routes.js
@@ -1,39 +1,47 @@
-import React from 'react';
-import { Icon } from '@chakra-ui/react';
+import React from "react";
+import { Icon } from "@chakra-ui/react";
 import {
-    MdBarChart,
-    MdHome,
-    MdTableChart,
-    MdEventNote,
-} from 'react-icons/md';
+  MdHome,
+  MdTableChart,
+  MdEventNote,
+  MdLocalOffer,
+} from "react-icons/md";
 
 // Admin Imports
-import MainDashboard from './views/dashboard/MainDashboard';
-import DataTables from './views/dataTables/DataTables';
-import TourManagement from './views/TourManagement';
+import MainDashboard from "./views/dashboard/MainDashboard";
+import DataTables from "./views/dataTables/DataTables";
+import TourManagement from "./views/TourManagement";
+import PromotionSettingPage from "./views/PromotionSettingPage";
 
 const routes = [
-    {
-        name: 'Dashboard',
-        layout: '/admin',
-        path: '/dashboard',
-        icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
-        component: <MainDashboard />,
-    },
-    {
-        name: 'Tours Details',
-        layout: '/admin',
-        icon: <Icon as={MdTableChart} width="20px" height="20px" color="inherit" />,
-        path: '/tours',
-        component: <DataTables />,
-    },
-    {
-        name: 'Tour Management',
-        layout: '/admin',
-        icon: <Icon as={MdEventNote} width="20px" height="20px" color="inherit" />,
-        path: '/tour-management',
-        component: <TourManagement />,
-    },
+  {
+    name: "Dashboard",
+    layout: "/admin",
+    path: "/dashboard",
+    icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
+    component: <MainDashboard />,
+  },
+  {
+    name: "Tours Details",
+    layout: "/admin",
+    path: "/tours",
+    icon: <Icon as={MdTableChart} width="20px" height="20px" color="inherit" />,
+    component: <DataTables />,
+  },
+  {
+    name: "Tour Management",
+    layout: "/admin",
+    path: "/tour-management",
+    icon: <Icon as={MdEventNote} width="20px" height="20px" color="inherit" />,
+    component: <TourManagement />,
+  },
+  {
+    name: "Promotion Settings", 
+    layout: "/admin",
+    path: "/promotion-settings",
+    icon: <Icon as={MdLocalOffer} width="20px" height="20px" color="inherit" />,
+    component: <PromotionSettingPage />,
+  },
 ];
 
 export default routes;
